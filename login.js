f2780977a5e1b1ee4681a87d9bf01ca9078ad4c8
unknown <sainathdhatti@gmail.com>
2022-11-30 13:35:36 +0530

    logindone

---
+++ login.js
@@ -0,0 +1,11 @@
+check given number is prime or not
+ let count=0
+    for(let i=1;i<=num;i++){
+        if(num%i==0){
+            count++
+        }
+    }if(count==2){
+        console.log("Yes")
+    }else{
+        console.log("No")
+    }
